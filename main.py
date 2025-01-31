9fe6f5c892cf1cbaf2d4e5f7b5c35c67152f00a4
Klemen <klemenpozlep@protonmail.com>
2025-01-31 16:39:13 +0100

    Keras and Tensorflow finally working now + import of twitter dataset

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import tensorflow as tf
+import keras
+from keras.src.layers import TextVectorization
+
 
 # test
 
-ggg
+# Uvoz naših twitter podatkov
+training_dataset = pd.read_csv('E:/School 2425/OSUPR/Projekt/archive/twitter_training.csv')
+validation_dataset = pd.read_csv('E:/School 2425/OSUPR/Projekt/archive/twitter_validation.csv')
+print(training_dataset.head())
+print(validation_dataset.info())
